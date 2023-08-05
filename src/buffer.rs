44e35671a0935ad82c10b93960c8667af09d5dc8
Matthijs Brobbel <m1brobbel@gmail.com>
2023-08-05 14:21:41 +0200

    Add `BufferType` implementation for `arrow_buffer::ScalarBuffer`

--- src/buffer.rs
+++ src/buffer.rs
@@ -362,7 +362,7 @@ impl<T: FixedSize> BufferMut<T> for Rc<[T]> {
 mod arrow {
     use super::{Buffer, BufferMut, BufferType};
     use crate::FixedSize;
-    use arrow_buffer::{ArrowNativeType, BufferBuilder};
+    use arrow_buffer::{ArrowNativeType, BufferBuilder, ScalarBuffer};
 
     /// A [BufferType] implementation for [arrow_buffer::BufferBuilder].
     pub struct ArrowBuffer;
@@ -380,6 +380,19 @@ mod arrow {
     impl<T: FixedSize + ArrowNativeType> BufferMut<T> for BufferBuilder<T> {
         fn as_mut_slice(&mut self) -> &mut [T] {
             BufferBuilder::as_slice_mut(self)
+        }
+    }
+
+    /// A [BufferType] implementation for [arrow_buffer::ScalarBuffer].
+    pub struct ArrowScalarBuffer;
+
+    impl BufferType for ArrowScalarBuffer {
+        type Buffer<T: FixedSize> = ScalarBuffer<T>;
+    }
+
+    impl<T: FixedSize + ArrowNativeType> Buffer<T> for ScalarBuffer<T> {
+        fn as_slice(&self) -> &[T] {
+            self
         }
     }
 }
@@ -465,5 +478,8 @@ mod tests {
         );
         <_ as BufferMut<u64>>::as_mut_slice(&mut buffer)[3] = 42;
         assert_eq!(<_ as Buffer<u64>>::as_slice(&buffer), &[1, 2, 3, 42]);
+
+        let buffer = arrow_buffer::ScalarBuffer::from_iter([1, 2, 3, 4]);
+        assert_eq!(<_ as Buffer<u32>>::as_slice(&buffer), &[1, 2, 3, 4]);
     }
 }
