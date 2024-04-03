2ab7e0436f6530289ebba38c4ce344570a26926f
Matthijs Brobbel <m1brobbel@gmail.com>
2024-04-03 17:28:19 +0200

    Update derive expand tests

--- narrow-derive/tests/expand/enum/named/generic.expanded.rs
+++ narrow-derive/tests/expand/enum/named/generic.expanded.rs
@@ -3,6 +3,273 @@ enum Foo<T> {
     Bar(T),
     None,
 }
+impl<T> ::std::convert::From<&Foo<T>> for ::std::primitive::i8 {
+    fn from(value: &Foo<T>) -> ::std::primitive::i8 {
+        match *value {
+            Foo::Foo { .. } => 0,
+            Foo::Bar(..) => 1,
+            Foo::None => 2,
+        }
+    }
+}
+struct FooVariantFoo<T> {
+    bar: T,
+}
+#[automatically_derived]
+impl<T: ::core::default::Default> ::core::default::Default for FooVariantFoo<T> {
+    #[inline]
+    fn default() -> FooVariantFoo<T> {
+        FooVariantFoo {
+            bar: ::core::default::Default::default(),
+        }
+    }
+}
+struct FooVariantBar<T>(T);
+#[automatically_derived]
+impl<T: ::core::default::Default> ::core::default::Default for FooVariantBar<T> {
+    #[inline]
+    fn default() -> FooVariantBar<T> {
+        FooVariantBar(::core::default::Default::default())
+    }
+}
+impl<T> narrow::array::union::EnumVariant<0> for Foo<T> {
+    type Data = FooVariantFoo<T>;
+    fn from_data(value: Self::Data) -> Self {
+        Self::Foo { bar: value.bar }
+    }
+}
+impl<T> narrow::array::union::EnumVariant<1> for Foo<T> {
+    type Data = FooVariantBar<T>;
+    fn from_data(value: Self::Data) -> Self {
+        Self::Bar(value.0)
+    }
+}
+impl<T> narrow::array::union::EnumVariant<2> for Foo<T> {
+    type Data = ();
+    fn from_data(value: Self::Data) -> Self {
+        Self::None
+    }
+}
+struct FooArray<
+    T: narrow::array::ArrayType,
+    Buffer: narrow::buffer::BufferType,
+    OffsetItem: narrow::offset::OffsetElement,
+    UnionLayout: narrow::array::UnionType,
+>(
+    <<Foo<
+        T,
+    > as narrow::array::union::EnumVariant<
+        0,
+    >>::Data as narrow::array::ArrayType<
+        <Foo<T> as narrow::array::union::EnumVariant<0>>::Data,
+    >>::Array<Buffer, OffsetItem, UnionLayout>,
+    <<Foo<
+        T,
+    > as narrow::array::union::EnumVariant<
+        1,
+    >>::Data as narrow::array::ArrayType<
+        <Foo<T> as narrow::array::union::EnumVariant<1>>::Data,
+    >>::Array<Buffer, OffsetItem, UnionLayout>,
+    <<Foo<
+        T,
+    > as narrow::array::union::EnumVariant<
+        2,
+    >>::Data as narrow::array::ArrayType<
+        <Foo<T> as narrow::array::union::EnumVariant<2>>::Data,
+    >>::Array<Buffer, OffsetItem, UnionLayout>,
+);
+impl<
+    T: narrow::array::ArrayType,
+    Buffer: narrow::buffer::BufferType,
+    OffsetItem: narrow::offset::OffsetElement,
+    UnionLayout: narrow::array::UnionType,
+> ::std::default::Default for FooArray<T, Buffer, OffsetItem, UnionLayout>
+where
+    <<Foo<
+        T,
+    > as narrow::array::union::EnumVariant<
+        0,
+    >>::Data as narrow::array::ArrayType<
+        <Foo<T> as narrow::array::union::EnumVariant<0>>::Data,
+    >>::Array<Buffer, OffsetItem, UnionLayout>: ::std::default::Default,
+    <<Foo<
+        T,
+    > as narrow::array::union::EnumVariant<
+        1,
+    >>::Data as narrow::array::ArrayType<
+        <Foo<T> as narrow::array::union::EnumVariant<1>>::Data,
+    >>::Array<Buffer, OffsetItem, UnionLayout>: ::std::default::Default,
+    <<Foo<
+        T,
+    > as narrow::array::union::EnumVariant<
+        2,
+    >>::Data as narrow::array::ArrayType<
+        <Foo<T> as narrow::array::union::EnumVariant<2>>::Data,
+    >>::Array<Buffer, OffsetItem, UnionLayout>: ::std::default::Default,
+{
+    fn default() -> Self {
+        Self(
+            ::std::default::Default::default(),
+            ::std::default::Default::default(),
+            ::std::default::Default::default(),
+        )
+    }
+}
+impl<
+    T: narrow::array::ArrayType,
+    Buffer: narrow::buffer::BufferType,
+    OffsetItem: narrow::offset::OffsetElement,
+> ::std::iter::Extend<Foo<T>> for FooArray<T, Buffer, OffsetItem, DenseLayout>
+where
+    <<Foo<
+        T,
+    > as narrow::array::union::EnumVariant<
+        0,
+    >>::Data as narrow::array::ArrayType<
+        <Foo<T> as narrow::array::union::EnumVariant<0>>::Data,
+    >>::Array<
+        Buffer,
+        OffsetItem,
+        narrow::array::DenseLayout,
+    >: ::std::iter::Extend<FooVariantFoo<T>>,
+    <<Foo<
+        T,
+    > as narrow::array::union::EnumVariant<
+        1,
+    >>::Data as narrow::array::ArrayType<
+        <Foo<T> as narrow::array::union::EnumVariant<1>>::Data,
+    >>::Array<
+        Buffer,
+        OffsetItem,
+        narrow::array::DenseLayout,
+    >: ::std::iter::Extend<FooVariantBar<T>>,
+    <<Foo<
+        T,
+    > as narrow::array::union::EnumVariant<
+        2,
+    >>::Data as narrow::array::ArrayType<
+        <Foo<T> as narrow::array::union::EnumVariant<2>>::Data,
+    >>::Array<Buffer, OffsetItem, narrow::array::DenseLayout>: ::std::iter::Extend<()>,
+{
+    fn extend<I>(&mut self, iter: I)
+    where
+        I: IntoIterator<Item = Foo<T>>,
+    {
+        iter.into_iter()
+            .for_each(|variant| {
+                match variant {
+                    Foo::Foo { bar } => {
+                        self.0.extend(::std::iter::once(FooVariantFoo { bar }));
+                    }
+                    Foo::Bar(_0) => {
+                        self.1.extend(::std::iter::once(FooVariantBar(_0)));
+                    }
+                    Foo::None => {
+                        self.2.extend(::std::iter::once(()));
+                    }
+                }
+            });
+    }
+}
+impl<
+    T: narrow::array::ArrayType,
+    Buffer: narrow::buffer::BufferType,
+    OffsetItem: narrow::offset::OffsetElement,
+> ::std::iter::Extend<Foo<T>> for FooArray<T, Buffer, OffsetItem, SparseLayout>
+where
+    <<Foo<
+        T,
+    > as narrow::array::union::EnumVariant<
+        0,
+    >>::Data as narrow::array::ArrayType<
+        <Foo<T> as narrow::array::union::EnumVariant<0>>::Data,
+    >>::Array<
+        Buffer,
+        OffsetItem,
+        narrow::array::SparseLayout,
+    >: ::std::iter::Extend<FooVariantFoo<T>>,
+    <<Foo<
+        T,
+    > as narrow::array::union::EnumVariant<
+        1,
+    >>::Data as narrow::array::ArrayType<
+        <Foo<T> as narrow::array::union::EnumVariant<1>>::Data,
+    >>::Array<
+        Buffer,
+        OffsetItem,
+        narrow::array::SparseLayout,
+    >: ::std::iter::Extend<FooVariantBar<T>>,
+    <<Foo<
+        T,
+    > as narrow::array::union::EnumVariant<
+        2,
+    >>::Data as narrow::array::ArrayType<
+        <Foo<T> as narrow::array::union::EnumVariant<2>>::Data,
+    >>::Array<Buffer, OffsetItem, narrow::array::SparseLayout>: ::std::iter::Extend<()>,
+{
+    fn extend<I>(&mut self, iter: I)
+    where
+        I: IntoIterator<Item = Foo<T>>,
+    {
+        iter.into_iter()
+            .for_each(|variant| {
+                match variant {
+                    Foo::Foo { bar } => {
+                        self.0.extend(::std::iter::once(FooVariantFoo { bar }));
+                        self.1
+                            .extend(
+                                ::std::iter::once(::std::default::Default::default()),
+                            );
+                        self.2
+                            .extend(
+                                ::std::iter::once(::std::default::Default::default()),
+                            );
+                    }
+                    Foo::Bar(_0) => {
+                        self.1.extend(::std::iter::once(FooVariantBar(_0)));
+                        self.0
+                            .extend(
+                                ::std::iter::once(::std::default::Default::default()),
+                            );
+                        self.2
+                            .extend(
+                                ::std::iter::once(::std::default::Default::default()),
+                            );
+                    }
+                    Foo::None => {
+                        self.2.extend(::std::iter::once(()));
+                        self.0
+                            .extend(
+                                ::std::iter::once(::std::default::Default::default()),
+                            );
+                        self.1
+                            .extend(
+                                ::std::iter::once(::std::default::Default::default()),
+                            );
+                    }
+                }
+            });
+    }
+}
+impl<T: narrow::array::ArrayType> narrow::array::UnionArrayType<3> for Foo<T> {
+    type Array<
+        Buffer: narrow::buffer::BufferType,
+        OffsetItem: narrow::offset::OffsetElement,
+        UnionLayout: narrow::array::UnionType,
+    > = FooArray<T, Buffer, OffsetItem, UnionLayout>;
+}
+impl<T: narrow::array::ArrayType> narrow::array::ArrayType<Foo<T>> for Foo<T> {
+    type Array<
+        Buffer: narrow::buffer::BufferType,
+        OffsetItem: narrow::offset::OffsetElement,
+        UnionLayout: narrow::array::UnionType,
+    > = narrow::array::UnionArray<
+        Self,
+        { <Self as narrow::array::UnionArrayType<3>>::VARIANTS },
+        UnionLayout,
+        Buffer,
+    >;
+}
 #[automatically_derived]
 impl<T: ::core::clone::Clone> ::core::clone::Clone for Foo<T> {
     #[inline]
--- narrow-derive/tests/expand/enum/unit/simple.expanded.rs
+++ narrow-derive/tests/expand/enum/unit/simple.expanded.rs
@@ -4,3 +4,264 @@ enum FooBar {
     FOO,
     Bar,
 }
+impl ::std::convert::From<&FooBar> for ::std::primitive::i8 {
+    fn from(value: &FooBar) -> ::std::primitive::i8 {
+        match *value {
+            FooBar::Foo => 0,
+            FooBar::FoO => 1,
+            FooBar::FOO => 2,
+            FooBar::Bar => 3,
+        }
+    }
+}
+impl narrow::array::union::EnumVariant<0> for FooBar {
+    type Data = ();
+    fn from_data(value: Self::Data) -> Self {
+        Self::Foo
+    }
+}
+impl narrow::array::union::EnumVariant<1> for FooBar {
+    type Data = ();
+    fn from_data(value: Self::Data) -> Self {
+        Self::FoO
+    }
+}
+impl narrow::array::union::EnumVariant<2> for FooBar {
+    type Data = ();
+    fn from_data(value: Self::Data) -> Self {
+        Self::FOO
+    }
+}
+impl narrow::array::union::EnumVariant<3> for FooBar {
+    type Data = ();
+    fn from_data(value: Self::Data) -> Self {
+        Self::Bar
+    }
+}
+struct FooBarArray<
+    Buffer: narrow::buffer::BufferType,
+    OffsetItem: narrow::offset::OffsetElement,
+    UnionLayout: narrow::array::UnionType,
+>(
+    <<FooBar as narrow::array::union::EnumVariant<
+        0,
+    >>::Data as narrow::array::ArrayType<
+        <FooBar as narrow::array::union::EnumVariant<0>>::Data,
+    >>::Array<Buffer, OffsetItem, UnionLayout>,
+    <<FooBar as narrow::array::union::EnumVariant<
+        1,
+    >>::Data as narrow::array::ArrayType<
+        <FooBar as narrow::array::union::EnumVariant<1>>::Data,
+    >>::Array<Buffer, OffsetItem, UnionLayout>,
+    <<FooBar as narrow::array::union::EnumVariant<
+        2,
+    >>::Data as narrow::array::ArrayType<
+        <FooBar as narrow::array::union::EnumVariant<2>>::Data,
+    >>::Array<Buffer, OffsetItem, UnionLayout>,
+    <<FooBar as narrow::array::union::EnumVariant<
+        3,
+    >>::Data as narrow::array::ArrayType<
+        <FooBar as narrow::array::union::EnumVariant<3>>::Data,
+    >>::Array<Buffer, OffsetItem, UnionLayout>,
+);
+impl<
+    Buffer: narrow::buffer::BufferType,
+    OffsetItem: narrow::offset::OffsetElement,
+    UnionLayout: narrow::array::UnionType,
+> ::std::default::Default for FooBarArray<Buffer, OffsetItem, UnionLayout>
+where
+    <<FooBar as narrow::array::union::EnumVariant<
+        0,
+    >>::Data as narrow::array::ArrayType<
+        <FooBar as narrow::array::union::EnumVariant<0>>::Data,
+    >>::Array<Buffer, OffsetItem, UnionLayout>: ::std::default::Default,
+    <<FooBar as narrow::array::union::EnumVariant<
+        1,
+    >>::Data as narrow::array::ArrayType<
+        <FooBar as narrow::array::union::EnumVariant<1>>::Data,
+    >>::Array<Buffer, OffsetItem, UnionLayout>: ::std::default::Default,
+    <<FooBar as narrow::array::union::EnumVariant<
+        2,
+    >>::Data as narrow::array::ArrayType<
+        <FooBar as narrow::array::union::EnumVariant<2>>::Data,
+    >>::Array<Buffer, OffsetItem, UnionLayout>: ::std::default::Default,
+    <<FooBar as narrow::array::union::EnumVariant<
+        3,
+    >>::Data as narrow::array::ArrayType<
+        <FooBar as narrow::array::union::EnumVariant<3>>::Data,
+    >>::Array<Buffer, OffsetItem, UnionLayout>: ::std::default::Default,
+{
+    fn default() -> Self {
+        Self(
+            ::std::default::Default::default(),
+            ::std::default::Default::default(),
+            ::std::default::Default::default(),
+            ::std::default::Default::default(),
+        )
+    }
+}
+impl<
+    Buffer: narrow::buffer::BufferType,
+    OffsetItem: narrow::offset::OffsetElement,
+> ::std::iter::Extend<FooBar> for FooBarArray<Buffer, OffsetItem, DenseLayout>
+where
+    <<FooBar as narrow::array::union::EnumVariant<
+        0,
+    >>::Data as narrow::array::ArrayType<
+        <FooBar as narrow::array::union::EnumVariant<0>>::Data,
+    >>::Array<Buffer, OffsetItem, narrow::array::DenseLayout>: ::std::iter::Extend<()>,
+    <<FooBar as narrow::array::union::EnumVariant<
+        1,
+    >>::Data as narrow::array::ArrayType<
+        <FooBar as narrow::array::union::EnumVariant<1>>::Data,
+    >>::Array<Buffer, OffsetItem, narrow::array::DenseLayout>: ::std::iter::Extend<()>,
+    <<FooBar as narrow::array::union::EnumVariant<
+        2,
+    >>::Data as narrow::array::ArrayType<
+        <FooBar as narrow::array::union::EnumVariant<2>>::Data,
+    >>::Array<Buffer, OffsetItem, narrow::array::DenseLayout>: ::std::iter::Extend<()>,
+    <<FooBar as narrow::array::union::EnumVariant<
+        3,
+    >>::Data as narrow::array::ArrayType<
+        <FooBar as narrow::array::union::EnumVariant<3>>::Data,
+    >>::Array<Buffer, OffsetItem, narrow::array::DenseLayout>: ::std::iter::Extend<()>,
+{
+    fn extend<I>(&mut self, iter: I)
+    where
+        I: IntoIterator<Item = FooBar>,
+    {
+        iter.into_iter()
+            .for_each(|variant| {
+                match variant {
+                    FooBar::Foo => {
+                        self.0.extend(::std::iter::once(()));
+                    }
+                    FooBar::FoO => {
+                        self.1.extend(::std::iter::once(()));
+                    }
+                    FooBar::FOO => {
+                        self.2.extend(::std::iter::once(()));
+                    }
+                    FooBar::Bar => {
+                        self.3.extend(::std::iter::once(()));
+                    }
+                }
+            });
+    }
+}
+impl<
+    Buffer: narrow::buffer::BufferType,
+    OffsetItem: narrow::offset::OffsetElement,
+> ::std::iter::Extend<FooBar> for FooBarArray<Buffer, OffsetItem, SparseLayout>
+where
+    <<FooBar as narrow::array::union::EnumVariant<
+        0,
+    >>::Data as narrow::array::ArrayType<
+        <FooBar as narrow::array::union::EnumVariant<0>>::Data,
+    >>::Array<Buffer, OffsetItem, narrow::array::SparseLayout>: ::std::iter::Extend<()>,
+    <<FooBar as narrow::array::union::EnumVariant<
+        1,
+    >>::Data as narrow::array::ArrayType<
+        <FooBar as narrow::array::union::EnumVariant<1>>::Data,
+    >>::Array<Buffer, OffsetItem, narrow::array::SparseLayout>: ::std::iter::Extend<()>,
+    <<FooBar as narrow::array::union::EnumVariant<
+        2,
+    >>::Data as narrow::array::ArrayType<
+        <FooBar as narrow::array::union::EnumVariant<2>>::Data,
+    >>::Array<Buffer, OffsetItem, narrow::array::SparseLayout>: ::std::iter::Extend<()>,
+    <<FooBar as narrow::array::union::EnumVariant<
+        3,
+    >>::Data as narrow::array::ArrayType<
+        <FooBar as narrow::array::union::EnumVariant<3>>::Data,
+    >>::Array<Buffer, OffsetItem, narrow::array::SparseLayout>: ::std::iter::Extend<()>,
+{
+    fn extend<I>(&mut self, iter: I)
+    where
+        I: IntoIterator<Item = FooBar>,
+    {
+        iter.into_iter()
+            .for_each(|variant| {
+                match variant {
+                    FooBar::Foo => {
+                        self.0.extend(::std::iter::once(()));
+                        self.1
+                            .extend(
+                                ::std::iter::once(::std::default::Default::default()),
+                            );
+                        self.2
+                            .extend(
+                                ::std::iter::once(::std::default::Default::default()),
+                            );
+                        self.3
+                            .extend(
+                                ::std::iter::once(::std::default::Default::default()),
+                            );
+                    }
+                    FooBar::FoO => {
+                        self.1.extend(::std::iter::once(()));
+                        self.0
+                            .extend(
+                                ::std::iter::once(::std::default::Default::default()),
+                            );
+                        self.2
+                            .extend(
+                                ::std::iter::once(::std::default::Default::default()),
+                            );
+                        self.3
+                            .extend(
+                                ::std::iter::once(::std::default::Default::default()),
+                            );
+                    }
+                    FooBar::FOO => {
+                        self.2.extend(::std::iter::once(()));
+                        self.0
+                            .extend(
+                                ::std::iter::once(::std::default::Default::default()),
+                            );
+                        self.1
+                            .extend(
+                                ::std::iter::once(::std::default::Default::default()),
+                            );
+                        self.3
+                            .extend(
+                                ::std::iter::once(::std::default::Default::default()),
+                            );
+                    }
+                    FooBar::Bar => {
+                        self.3.extend(::std::iter::once(()));
+                        self.0
+                            .extend(
+                                ::std::iter::once(::std::default::Default::default()),
+                            );
+                        self.1
+                            .extend(
+                                ::std::iter::once(::std::default::Default::default()),
+                            );
+                        self.2
+                            .extend(
+                                ::std::iter::once(::std::default::Default::default()),
+                            );
+                    }
+                }
+            });
+    }
+}
+impl narrow::array::UnionArrayType<4> for FooBar {
+    type Array<
+        Buffer: narrow::buffer::BufferType,
+        OffsetItem: narrow::offset::OffsetElement,
+        UnionLayout: narrow::array::UnionType,
+    > = FooBarArray<Buffer, OffsetItem, UnionLayout>;
+}
+impl narrow::array::ArrayType<FooBar> for FooBar {
+    type Array<
+        Buffer: narrow::buffer::BufferType,
+        OffsetItem: narrow::offset::OffsetElement,
+        UnionLayout: narrow::array::UnionType,
+    > = narrow::array::UnionArray<
+        Self,
+        { <Self as narrow::array::UnionArrayType<4>>::VARIANTS },
+        UnionLayout,
+        Buffer,
+    >;
+}
