fd9078897b32eef7e1d49935e3c267621a6d43eb
Johan Peltenburg <johan.peltenburg+code@gmail.com>
2024-10-02 08:42:27 +0200

    feat: Add benchmarks for primitive array building from an iterator (#255)
    Signed-off-by: Johan Peltenburg <johan.peltenburg+code@gmail.com>

--- benches/narrow/main.rs
+++ benches/narrow/main.rs
@@ -1,11 +1,13 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
 mod bitmap;
+mod versus;
 
 criterion_group! {
   name = narrow;
   config = Criterion::default();
   targets =
-    bitmap::bench
+    bitmap::bench,
+    versus::arrow::primitive::bench,
 }
 criterion_main!(narrow);
