1ca562646bf484577b28605bb66893f6ae805a73
Matthijs Brobbel <m1brobbel@gmail.com>
2026-07-23 11:46:57 +0200

    build: use rust-overlay for Nix toolchains (#617)

--- flake.nix
+++ flake.nix
@@ -1,13 +1,41 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = { nixpkgs, ... }:
+  outputs = { nixpkgs, rust-overlay, ... }:
     let
-      system = builtins.currentSystem;
-      pkgs = import nixpkgs { inherit system; };
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
     in {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [ pkgs.rustc pkgs.cargo pkgs.clippy pkgs.rustfmt ];
-      };
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay.overlays.default ];
+          };
+          stable = pkgs.mkShell {
+            packages = [ pkgs.rust-bin.stable.latest.default ];
+          };
+        in {
+          default = stable;
+          inherit stable;
+          nightly = pkgs.mkShell {
+            packages = [
+              (pkgs.rust-bin.selectLatestNightlyWith (toolchain:
+                toolchain.default.override {
+                  extensions = [ "miri" "rust-src" ];
+                }))
+            ];
+          };
+        });
     };
 }
